5772ee6cbcf146deb42edcf20dbe96363882413f
Anggih Septiawan <anggih.septiawan@accelbyte.net>
2022-10-16 15:51:53 +0700

    chore: add sitemap generator to boost SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // lib to add robot.txt
-import robotsTxt from "astro-robots-txt"
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://abbasbengkel.vercel.app/",
-  integrations: [tailwind(), robotsTxt()],
-})
+  integrations: [tailwind(), robotsTxt(), sitemap()]
+});
