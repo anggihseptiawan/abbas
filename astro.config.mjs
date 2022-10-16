1ad7a6c2a913874695f4f5a867232e1b853506d5
Anggih Septiawan <anggih.septiawan@accelbyte.net>
2022-10-16 15:41:54 +0700

    chore: add robot txt generator to boost SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+// lib to add robot.txt
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  site: "https://abbasbengkel.vercel.app/",
+  integrations: [tailwind(), robotsTxt()],
+})
